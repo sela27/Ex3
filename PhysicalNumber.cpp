aa4dec367cc970204d82137d253d76af5c9e22af
sela27 <sela@goldenberg.xyz>
2019-04-06 20:51:05 +0300

    finish operators first try

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -7,156 +7,253 @@ using ariel::Unit;
 //using namespace ariel;
 using namespace std;
 
-PhysicalNumber::PhysicalNumber(double size, Unit type)
-{
-	_size = size;
-	_type = type;
-}
 
-PhysicalNumber::PhysicalNumber()
-{
-	_size = 0;
-	_type = Unit::KM;
-}
-
-PhysicalNumber::~PhysicalNumber()
-{
-}
-
-
-
-PhysicalNumber PhysicalNumber::operator+(const PhysicalNumber& other)
-{
-	PhysicalNumber convert = Convert(other);
-	return PhysicalNumber(convert._size + _size, _type);
-
-}
-
-PhysicalNumber PhysicalNumber::Convert(const PhysicalNumber& other)
-{
-	if (_type == other._type)
+	PhysicalNumber::PhysicalNumber(double size, Unit type)
 	{
-		return other;
+		_size = size;
+		_type = type;
 	}
-	else if ((_type < 3 && other._type > 3) || ((_type > 3 && _type < 7) && (other._type < 3 || other._type > 7)) || (_type > 7 && other._type < 7))
+
+	PhysicalNumber::PhysicalNumber()
 	{
-		
-		throw string("Cannot convert " + to_string( _type) + "to " + to_string( other._type));
+		_size = 0;
+		_type = Unit::KM;
 	}
-	else
+
+	PhysicalNumber::~PhysicalNumber()
 	{
-		switch (_type)
+	}
+
+	double ariel::PhysicalNumber::getSize()
+	{
+		return _size;
+	}
+
+	Unit ariel::PhysicalNumber::getType()
+	{
+		return _type;
+	}
+
+
+
+	PhysicalNumber PhysicalNumber::operator+(const PhysicalNumber& other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return PhysicalNumber(convert._size + _size, _type);
+
+	}
+
+	PhysicalNumber PhysicalNumber::operator+=(const PhysicalNumber& other)
+	{
+		PhysicalNumber convert = Convert(other);
+		_size += convert._size;
+		return *this;
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator+()
+	{
+		return *this;
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator-(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return PhysicalNumber(convert._size - _size, _type);
+
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator-=(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		_size -= convert._size;
+		return *this;
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator-()
+	{
+		_size = -1 * _size;
+		return *this;
+	}
+
+	bool ariel::PhysicalNumber::operator<(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size < convert._size;
+	}
+
+	bool ariel::PhysicalNumber::operator>(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size > convert._size;
+	}
+
+	bool ariel::PhysicalNumber::operator<=(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size <= convert._size;
+	}
+
+	bool ariel::PhysicalNumber::operator>=(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size >= convert._size;
+	}
+
+	bool ariel::PhysicalNumber::operator==(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size == convert._size;
+	}
+
+	bool ariel::PhysicalNumber::operator!=(const PhysicalNumber & other)
+	{
+		PhysicalNumber convert = Convert(other);
+		return _size != convert._size;
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator++()
+	{
+		_size++;
+		return *this;
+	}
+
+	PhysicalNumber ariel::PhysicalNumber::operator--()
+	{
+		_size--;
+		return *this;
+	}
+
+	/*std::ostream& operator<<(ostream& os, PhysicalNumber& other)
+	{
+		return (os << other.getSize() << '[' << other.getType() << ']');
+	}*/
+
+	PhysicalNumber PhysicalNumber::Convert(const PhysicalNumber& other)
+	{
+		if (_type == other._type)
 		{
-		case 0: 
-		{
-			switch (other._type)
-			{
-			case 1: return PhysicalNumber(other._size * 1000, _type);
-				break;
-			case 2: return PhysicalNumber(other._size / 100, _type);
-			default:
-				break;
-			}
+			return other;
 		}
-		break;
-		case 1:
+		else if ((_type < 3 && other._type > 3) || ((_type > 3 && _type < 7) && (other._type < 3 || other._type > 7)) || (_type > 7 && other._type < 7))
 		{
-			switch (other._type)
-			{
-			case 0: return PhysicalNumber(other._size / 1000, _type);
-				break;
-			case 2: return PhysicalNumber(other._size / 100000, _type);
-			default:
-				break;
-			}
+
+			throw string("Cannot convert " + to_string(_type) + "to " + to_string(other._type));
 		}
-		break;
-		case 2:
+		else
 		{
-			switch (other._type)
+			switch (_type)
 			{
-			case 0: return PhysicalNumber(other._size * 100, _type);
-				break;
-			case 1: return PhysicalNumber(other._size * 100000, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		case 3:
-		{
-			switch (other._type)
+			case 0:
 			{
-			case 4: return PhysicalNumber(other._size * 60, _type);
-				break;
-			case 5: return PhysicalNumber(other._size * 3600, _type);
-			default:
-				break;
+				switch (other._type)
+				{
+				case 1: return PhysicalNumber(other._size * 1000, _type);
+					break;
+				case 2: return PhysicalNumber(other._size / 100, _type);
+				default:
+					break;
+				}
 			}
-		}
-		break;
-		case 4:
-		{
-			switch (other._type)
-			{
-			case 3: return PhysicalNumber(other._size / 60, _type);
-				break;
-			case 5: return PhysicalNumber(other._size * 60, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		case 5:
-		{
-			switch (other._type)
-			{
-			case 3: return PhysicalNumber(other._size / 3600, _type);
-				break;
-			case 4: return PhysicalNumber(other._size / 60, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		case 6:
-		{
-			switch (other._type)
-			{
-			case 7: return PhysicalNumber(other._size * 1000, _type);
-				break;
-			case 8: return PhysicalNumber(other._size * 1000000, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		case 7:
-		{
-			switch (other._type)
-			{
-			case 6: return PhysicalNumber(other._size / 1000, _type);
-				break;
-			case 8: return PhysicalNumber(other._size * 1000, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		case 8:
-		{
-			switch (other._type)
-			{
-			case 6: return PhysicalNumber(other._size / 1000000, _type);
-				break;
-			case 7: return PhysicalNumber(other._size / 1000, _type);
-			default:
-				break;
-			}
-		}
-		break;
-		default:
 			break;
+			case 1:
+			{
+				switch (other._type)
+				{
+				case 0: return PhysicalNumber(other._size / 1000, _type);
+					break;
+				case 2: return PhysicalNumber(other._size / 100000, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 2:
+			{
+				switch (other._type)
+				{
+				case 0: return PhysicalNumber(other._size * 100, _type);
+					break;
+				case 1: return PhysicalNumber(other._size * 100000, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 3:
+			{
+				switch (other._type)
+				{
+				case 4: return PhysicalNumber(other._size * 60, _type);
+					break;
+				case 5: return PhysicalNumber(other._size * 3600, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 4:
+			{
+				switch (other._type)
+				{
+				case 3: return PhysicalNumber(other._size / 60, _type);
+					break;
+				case 5: return PhysicalNumber(other._size * 60, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 5:
+			{
+				switch (other._type)
+				{
+				case 3: return PhysicalNumber(other._size / 3600, _type);
+					break;
+				case 4: return PhysicalNumber(other._size / 60, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 6:
+			{
+				switch (other._type)
+				{
+				case 7: return PhysicalNumber(other._size * 1000, _type);
+					break;
+				case 8: return PhysicalNumber(other._size * 1000000, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 7:
+			{
+				switch (other._type)
+				{
+				case 6: return PhysicalNumber(other._size / 1000, _type);
+					break;
+				case 8: return PhysicalNumber(other._size * 1000, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			case 8:
+			{
+				switch (other._type)
+				{
+				case 6: return PhysicalNumber(other._size / 1000000, _type);
+					break;
+				case 7: return PhysicalNumber(other._size / 1000, _type);
+				default:
+					break;
+				}
+			}
+			break;
+			default:
+				break;
+			}
 		}
 	}
-}
+
--- PhysicalNumber.hpp
+++ PhysicalNumber.hpp
@@ -4,17 +4,42 @@
 
 namespace ariel
 {
+
 	class PhysicalNumber
 	{
-	public:
-		PhysicalNumber(double size , Unit type);
-		PhysicalNumber();
-		~PhysicalNumber();
-		PhysicalNumber operator+(const PhysicalNumber& other);
-
 	private:
 		double _size;
 		Unit _type;
 		PhysicalNumber Convert(const PhysicalNumber& other);
+
+
+	public:
+		PhysicalNumber(double size, Unit type);
+		PhysicalNumber();
+		~PhysicalNumber();
+		double getSize();
+		Unit getType();
+		PhysicalNumber operator+(const PhysicalNumber& other);
+		PhysicalNumber operator+=(const PhysicalNumber& other);
+		PhysicalNumber operator+();
+		PhysicalNumber operator-(const PhysicalNumber& other);
+		PhysicalNumber operator-=(const PhysicalNumber& other);
+		PhysicalNumber operator-();
+		bool operator<(const PhysicalNumber& other);
+		bool operator>(const PhysicalNumber& other);
+		bool operator<=(const PhysicalNumber& other);
+		bool operator>=(const PhysicalNumber& other);
+		bool operator==(const PhysicalNumber& other);
+		bool operator!=(const PhysicalNumber& other);
+		friend std::ostream& operator<<(ostream& os, const PhysicalNumber& other)
+		{
+			return (os << other._size << '[' << other._type << ']');
+		}
+		friend std::ostream& operator>>(ostream& os, const PhysicalNumber& other)
+		{
+
+		}
+		PhysicalNumber operator++();
+		PhysicalNumber operator--();
 	};
 }
