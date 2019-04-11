5a15dee0408967426cf1ec8db32a603661ead30e
sela27 <sela@goldenberg.xyz>
2019-04-11 17:26:59 +0300

    adding print in << for testing in badkan

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -150,6 +150,7 @@ using namespace std;
 			string Ssize = s.substr(0 , s.find('['));
 			double size = stod(Ssize);
 			string Sunit = s.substr(s.find('[') + 1 , s.find(']') - s.find('[') - 1);
+			cout << Sunit << endl;
 			Unit type = string_to_unit(Sunit);
 			other._size = size;
 			other._type = type;
